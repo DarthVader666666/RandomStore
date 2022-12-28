949505ddf958417dc8d0281f55e5ebd394cb2f7d
Darth Vader <rumyancer@gmail.com>
2022-12-28 16:10:32 +0100

    Created RandomStoreDB Context

---
+++ RandomStoreRepo/RandomStoreOneDB.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RandomStoreRepo
+{
+    public class RandomStoreOneDB: DbContext
+    {
+        public RandomStoreOneDB(DbContextOptions<RandomStoreOneDB> options) : base(options)
+        { 
+            this.Database.EnsureCreated();
+        }
+
+        public virtual DbSet<Product> Products { get; set; }
+        public virtual DbSet<Category> Categories { get; set; } 
+        public virtual DbSet<Order> Orders { get; set; }
+    }
+}
